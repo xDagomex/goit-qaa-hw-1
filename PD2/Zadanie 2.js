905e0a8ab532ae0a3991c91296da33fac80be9a4
xDagomex <adaskubiak@wp.pl>
2024-09-20 04:59:09 +0200

    12

---
+++ PD2/Zadanie 2.js
@@ -0,0 +1,9 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+const numbers = [1, 2, 3, 5, 7, 11, 13, 17, 19];
+
+for (let i = 0; i < numbers.length; i +=1) {
+    if (numbers[i] > 5) {
+        console.log(numbers[i])
+    }
+}
