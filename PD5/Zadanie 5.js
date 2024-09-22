f2cdc4d0fb501e43337eb879c6edd0e420fb3b63
xDagomex <adaskubiak@wp.pl>
2024-09-22 04:02:00 +0200

    20

--- PD5/Zadanie 5.js
+++ PD5/Zadanie 5.js
@@ -1,11 +1,18 @@
-const doubleNumber = (numbers) => {
-    numbers.map((number) => number *2)
-    return doubledArray
-}
+// const doubleNumber = (numbers) => {
+//     numbers.map((number) => number *2)
+//     return doubledArray
+// }
 
-const tab = [1, 2, 3, 5, 8, 13, 21, 34, 55]
+// const tab = [1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-doubleNumber(tab);
+// doubleNumber(tab);
 
-console.log(tab);
-console.log(doubledArray);
+const students = [
+    {name: "Bonifacy", year: "1", score: 42},
+    {name: "Gus", year: "1", score: 96},
+    {name: "Jaca", year: "3", score: 21},
+    {name: "Pawel", year: "5", score: 37},
+    {name: "Stan", year: "5", score: 31},
+];
+const name = students.map(student => student.name);
+console.log(name);
