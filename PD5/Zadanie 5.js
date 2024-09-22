dc5a427dc558739e3b8981cd08323ce10b5ea569
xDagomex <adaskubiak@wp.pl>
2024-09-22 03:43:23 +0200

    15

---
+++ PD5/Zadanie 5.js
@@ -0,0 +1,11 @@
+const doubleNumber = (numbers) => {
+    numbers.map((number) => number *2)
+    return dNumbers
+}
+
+const tab = [1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+doubleNumber(tab);
+
+console.log(tab);
+console.log(dNumbers);
