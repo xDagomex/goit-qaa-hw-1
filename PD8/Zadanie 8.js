30065aea1d3283941db28bdc4b6f5671a5328b11
xDagomex <adaskubiak@wp.pl>
2024-09-23 16:21:26 +0200

    38

---
+++ PD8/Zadanie 8.js
@@ -0,0 +1,13 @@
+const user = {
+    "name": "Josh", // String
+    "weight": 175, // Number
+    "age": 30, // Number
+    "eyecolor": "brown", // String
+    "isHappy": true, // Boolean
+    "cars": ["Chevy", "Honda"], // Array
+    "favoriteBook": {
+        "title": "The Last Kingdom",
+        "author": "Bernard Cornwell",
+        "rating": 8.38
+    } // Object
+}
