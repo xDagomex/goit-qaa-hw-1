52466d2b5660664ca28b335d02d1b5b1f2c696d0
xDagomex <adaskubiak@wp.pl>
2024-09-23 17:51:39 +0200

    40

--- PD8/Zadanie 8.js
+++ PD8/Zadanie 8.js
@@ -1,4 +1,4 @@
-//const json = '{"name": "Josh", "weight": 175, "age": 30, "eyecolor": "brown", "isHappy": true, "cars": ["Chevy", "Honda"], "favoriteBook": {"title": "The Last Kingdom", "author": "Bernard Cornwell", "rating": 8.38}}';
+const user = '{"name": "Josh", "weight": 175, "age": 30, "eyecolor": "brown", "isHappy": true, "cars": ["Chevy", "Honda"], "favoriteBook": {"title": "The Last Kingdom", "author": "Bernard Cornwell", "rating": 8.38}}';
 // name String
 // weight Number
 // age Number
@@ -17,3 +17,17 @@ console.log(metoda);
 console.log(metoda.name);
 console.log(metoda.age);
 console.log(metoda.isHappy);
+
+///////////////////////////////////////////////////////////////////////////
+
+console.log("Test 1");
+console.log("Test 2");
+
+try {
+    const dane = JSON.parse('{"name": "Mango", "age: 3, isHappy: true}');
+} catch (error) {
+    console.error("Błąd, nieprawidłowy ciąg JSON");
+}
+
+console.log("Test 3");
+console.log("Test 4");
