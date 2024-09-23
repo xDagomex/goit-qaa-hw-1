11a0f7e975ce9fe9fddbabd2590baaab769231f7
xDagomex <adaskubiak@wp.pl>
2024-09-23 16:45:59 +0200

    39

--- PD8/Zadanie 8.js
+++ PD8/Zadanie 8.js
@@ -1,13 +1,19 @@
-const user = {
-    "name": "Josh", // String
-    "weight": 175, // Number
-    "age": 30, // Number
-    "eyecolor": "brown", // String
-    "isHappy": true, // Boolean
-    "cars": ["Chevy", "Honda"], // Array
-    "favoriteBook": {
-        "title": "The Last Kingdom",
-        "author": "Bernard Cornwell",
-        "rating": 8.38
-    } // Object
-}
+//const json = '{"name": "Josh", "weight": 175, "age": 30, "eyecolor": "brown", "isHappy": true, "cars": ["Chevy", "Honda"], "favoriteBook": {"title": "The Last Kingdom", "author": "Bernard Cornwell", "rating": 8.38}}';
+// name String
+// weight Number
+// age Number
+// eyecolor String
+// isHappy Boolean
+// cars Array
+// favoriteBook Object
+// JSON jest podobny do obiektu, jest jego reprezentacją w postaci ciągu
+// Jego klucze są zawsze ciągami w podwójnych cudzysłowach, tak samo jak wartości
+// Wartości właściwości: string, number, object, array, boolean, null
+
+const json = '{"name": "Mango", "age": 3, "isHappy": true}';
+
+const metoda = JSON.parse(json);
+console.log(metoda);
+console.log(metoda.name);
+console.log(metoda.age);
+console.log(metoda.isHappy);
