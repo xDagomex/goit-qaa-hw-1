9d26f977ba9478ac9bb502fdf14b3c6381ed1c11
xDagomex <adaskubiak@wp.pl>
2024-09-23 02:21:27 +0200

    32

--- PD6/Zadanie 6.js
+++ PD6/Zadanie 6.js
@@ -1,18 +1,36 @@
 //Tworzenie obiektu
 const maciej = {
     username: "Maciej",
-    //Metoda showName używająca this
+    //Metoda, która zapisuje właściwość username w konsoli, używając słowa kluczowego this
     showName() {
         console.log(this.username);
     },
 };
-// Przetestowanie obiektu używając metody showName()
+// Przetestowanie obiektu wywołując metodę showName()
 maciej.showName();
 
-const authors = [
-    { name: "Tolkien" },
-    { name: "Sapkowski" },
-    { name: "Sanderson" },
-    { name: "Dukaj" },
-    { name: "Gaiman"}
-];
+//////////////////////////////////////////////////////////////////
+
+// Tworzenie obiektu
+const bookshelf = {
+    // Tworzenie tablicy do przechowywania autorów
+    authors: [], 
+    // Metoda zwracająca tablicę autorów przechowywanych na półce z książkami
+    getAuthors: function() {
+      return this.authors; // Odnoszę się do autorów w tym obiekcie: this.authors
+    },
+    // Metoda, która przyjmuje imię autora jako argument i dodaje je do tablicy autorów na półce z książkami
+    addAuthor: function(authorName) {
+      this.authors.push(authorName); // Dodaję autorów do tego obiektu: this.authors.push
+    }
+  };
+  
+  // Przykładowi autorzy dodani do obiektu
+  bookshelf.addAuthor("Tolkien");
+  bookshelf.addAuthor("Sapkowski");
+  bookshelf.addAuthor("Sanderson");
+  bookshelf.addAuthor("Dukaj");
+  bookshelf.addAuthor("Gaiman");
+  // Sprawdzenie, czy autorzy zostali dodani do obiektu
+  console.log(bookshelf.getAuthors());
+  
